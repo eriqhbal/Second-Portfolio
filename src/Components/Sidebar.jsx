b452e82620d00a3feac25407fa282956bf8f117a
Zuleriqhbal <eriqhbal@gmail.com>
2023-02-13 10:16:11 +0700

    Add config scss style

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+// Icons
+import { FiDownloadCloud } from 'react-icons/fi';
+import { RiMoneyCnyCircleLine } from 'react-icons/ri';
 
 // Images
-import  Profil  from '../Assets/Images/Profil.png'
+import Profil from '../Assets/Images/Profil.png'
+
+// Data
+import { socialIcon, side } from '../Data/data';
+
+const Progress = ({ title, done, back }) => {
+   return (
+      <div className='progress'>
+         <div className={`progress done ${back}`} style={{ opacity: 1, width: `${done}%` }}>
+         </div>
+         <div className='progress-title'>
+            <h4>{title}.</h4>
+         </div>
+         <div className='progress-num'>
+            <h4>{done}.</h4>
+         </div>
+      </div>
+   )
+}
 
 const Sidebar = () => {
    return (
@@ -9,7 +31,40 @@ const Sidebar = () => {
          <section className='sideContent'>
             <div className='sideContent-top'>
                <div className='sideContent-top-image'>
-                  <img style={{width: "10em"}} src={Profil} alt="/main-image" />
+                  <img style={{ width: "10em" }} src={Profil} alt="/main-image" />
+                  <div className="sideContent-top-image-name">
+                     <h3>Zuleriqhbal Hendri</h3>
+                     <span>Junior Developer</span>
+                  </div>
+               </div>
+               <div className="sideContent-top-social">
+                  {socialIcon.map((item, index) => (
+                     <div key={index} className={item.class}>
+                        <span>{item.icon}</span>
+                     </div>
+                  ))}
+               </div>
+               <div className="sideContent-bottom">
+                  <div className="sideContent-bottom-skill">
+                     {side.map(item => {
+                        return <div key={item.id} className='sideContent-Skills'>
+                           <Progress title={item.text} done={item.num} back={item.text} />
+                        </div>
+                     })}
+                  </div>
+               </div>
+               <div className="sideContent-bottom-buttonGroup">
+                  <button type="" className='sm-button'>
+                     <span>
+                        <FiDownloadCloud className="button_bicon" />
+                     </span>
+                  </button>
+                  <button type="" className='button'>
+                     <span>
+                        <RiMoneyCnyCircleLine className="button_bicon" />
+                        <span>HIRE ME</span>
+                     </span>
+                  </button>
                </div>
             </div>
          </section>
